6689cb9671290584f770823c82e0fc4e08ba9b6f
Dimitar Nikolov <dimitar@doonloo.com>
2018-03-21 12:59:58 +0200

    Create MainController for handling all requests and sending same HTML

---
+++ src/main/java/org/elsys/qtask/QTaskApplication.java
@@ -0,0 +1,12 @@
+package org.elsys.qtask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QTaskApplication.class, args);
+	}
+}
